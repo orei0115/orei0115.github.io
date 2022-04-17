f84ca8fc0d7552977feb5585871f1022cb740823
jennoreilly <orei0115@algonquinlive.com>
2022-04-17 15:08:31 -0400

    added basic validation of email and firstname lastname

--- lib2024/final/js/scripts.js
+++ lib2024/final/js/scripts.js
@@ -1,8 +1,19 @@
 function updateProfileCard() {
-	document.getElementById("profile-name").innerHTML=document.getElementById("name").value;
-	document.getElementById("profile-email").innerHTML=document.getElementById("email").value;
-	document.getElementById("profile-address").innerHTML=document.getElementById("address").value;
-	document.getElementById("profile-city").innerHTML=document.getElementById("city").value;
-	document.getElementById("profile-province").innerHTML=document.getElementById("province").value;
-	document.getElementById("profile-postalcode").innerHTML=document.getElementById("postal").value;
+	var regName = /^[a-zA-Z]+ [a-zA-Z]+$/;
+	var name = document.getElementById('name').value;
+	var regEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+	var email = document.getElementById("email").value;
+	if (!regName.test(name)){
+		alert('Please Enter Both your First Name and Your Last Name.');
+	} else if (!regEmail.test(email)) {
+		alert('Please Enter a valid email address.');
+	} else {
+		document.getElementById("profile-name").innerHTML=name;
+		document.getElementById("profile-email").innerHTML=email;
+		document.getElementById("profile-address").innerHTML=document.getElementById("address").value;
+		document.getElementById("profile-city").innerHTML=document.getElementById("city").value;
+		document.getElementById("profile-province").innerHTML=document.getElementById("province").value;
+		document.getElementById("profile-postalcode").innerHTML=document.getElementById("postal").value;
+	}
+
 }
